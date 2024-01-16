7bf773ac28a6b1f71ae50e82744834dd71844149
ashishkamblecctech <149309502+ashishkamblecctech@users.noreply.github.com>
2024-01-16 11:04:16 +0530

    Update StatsDataReader.h

--- StatsDataReader/StatsDataReader.h
+++ StatsDataReader/StatsDataReader.h
@@ -6,7 +6,7 @@ struct RegionInfo1 {
 	std::string filePath;
 	float r, g, b;
 	long statePopulation;
-		float stateEducation, stateArea;
+	float stateEducation, stateArea;
 };
 
 // This class is exported from the dll
@@ -14,11 +14,12 @@ class STATSDATAREADER_API StatsDataReader {
 public:
 	StatsDataReader();
 	~StatsDataReader();
-	// TODO: add your methods here.
 
 public:
-	void dataReader(std::vector <std::string>& stateNames, std::vector<long>& statePopulation,
-						std::vector<float>& stateEducation, std::vector<float>& stateArea, std::vector<long>& stateActualPopulation,
-							std::vector<float>& stateActualEducation, std::vector<float>& stateActualArea, std::vector <RegionInfo1>& reg);
+	void dataReader(std::vector <std::string>& stateNames, std::vector <RegionInfo1>& region1);
 
+private:
+	std::vector <long> statePopulation;
+	std::vector <float> stateEducation;
+	std::vector <float>	stateArea;
 };
