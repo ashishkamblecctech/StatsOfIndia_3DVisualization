62476f3daa83855e34f281ed05495188b1351b44
ashishkamblecctech <149309502+ashishkamblecctech@users.noreply.github.com>
2024-01-16 11:06:46 +0530

    Update StatsOfIndia.cpp

--- StatsOfIndia/src/StatsOfIndia.cpp
+++ StatsOfIndia/src/StatsOfIndia.cpp
@@ -21,6 +21,8 @@ void StatsOfIndia::setupUi()
 
     // UI part can be added in the code below
     resize(1900, 1000);
+    this->setWindowTitle("Stats of India - 3D Visualization");
+
     mCentralWidget = new QWidget(this);
 
     mGridLayoutWidget = new QWidget(mCentralWidget);
@@ -28,93 +30,83 @@ void StatsOfIndia::setupUi()
     mGridLayout = new QGridLayout(mGridLayoutWidget);
 
     mVerticalLayout = new QVBoxLayout();
-
     mHorizontalLayout = new QHBoxLayout();
 
     mComboBox = new QComboBox(mGridLayoutWidget);
-    mComboBox->addItem("Select Parameter:");
-    mComboBox->addItem("Population");
-    mComboBox->addItem("Education");
-    mComboBox->addItem("Area");
+
+    mComboBox->addItem("Select any parameters:");
+
+    QIcon populationIcon("Icons/population_icon.png");
+    mComboBox->addItem(populationIcon, "Population");
+
+    QIcon educationIcon("Icons/education_icon.png");
+    mComboBox->addItem(educationIcon, "Education");
+
+    QIcon areaIcon("Icons/area_icon.png");
+    mComboBox->addItem(areaIcon, "Area");
+
     mComboBox->setSizeAdjustPolicy(QComboBox::AdjustToContents); // To make page responsive
-    
+
+    mComboBox->setStyleSheet("QComboBox { background-color: white; border: 2px solid darkblue; padding: 5px; }");
+
     mVerticalLayout->addWidget(mComboBox);
 
     mTableView = new QTableView(mGridLayoutWidget);
     mTableView->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch); // To make page responsive
     QSizePolicy sizePolicy(QSizePolicy::Maximum, QSizePolicy::Expanding);
     mTableView->setSizePolicy(sizePolicy);
+    mTableView->setStyleSheet("QTableView { background-color: white; }"
+        "QHeaderView::section { background-color: lightblue; border: 1px solid darkblue; }"
+        "QTableView::item { border: 1px solid darkblue; }");
     mVerticalLayout->addWidget(mTableView);
 
     mGridLayout->addLayout(mVerticalLayout, 0, 1, 1, 1);
-
-    // Set the size policy for making the page responsive
     mCentralWidget->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-
-    // Set the layout for making the page responsive
     mCentralWidget->setLayout(mGridLayout);
-
     setCentralWidget(mCentralWidget);
-
-    // No need for additional layout, just add mOpenGLWidget directly
     mOpenGLWidget = new OpenGLWindow(QColor(0, 0, 0), mCentralWidget);
     mGridLayout->addWidget(mOpenGLWidget, 0, 0, 2, 1); // Spanning two rows
 }
 
 void StatsOfIndia::setupTableModel(const QStringList& stateNames, const QList<long>& stateValues)
 {
-    // Create a standard item model
     QStandardItemModel* model = new QStandardItemModel(this);
-
-    // Set the column headers
     model->setHorizontalHeaderLabels(QStringList() << "State Names" << "Values");
 
     for (int i = 0; i < stateNames.size(); i++) {
         QList<QStandardItem*> row;
-
-        // Add state name to the first column
-        row << new QStandardItem(stateNames[i]);
-
-        // Add the corresponding value for the second column
-        row << new QStandardItem(QString::number(stateValues[i]));
-
+        row << new QStandardItem(stateNames[i]); // Add state name to the first column
+        row << new QStandardItem(QString::number(stateValues[i])); // Add the corresponding value for the second column
         model->appendRow(row);
     }
 
-    // Set the model for the table view
-    mTableView->setModel(model);
+    mTableView->setModel(model); // Set the model for the table view
 }
 
 void StatsOfIndia::handleComboBoxChange(int index) {
 
-    // Fix the size of array to 31
-    stateNames.resize(31); 
-    statePopulation.resize(31);
-    stateEducation.resize(31);
-    stateArea.resize(31);
-    stateActualPopulation.resize(31);
-    stateActualEducation.resize(31);
-    stateActualArea.resize(31);
+    stateNames.resize(31); // Fix the size of array to 31
 
-    //Calling the reader and gathering all the data from the reader
     StatsDataReader reader;
-    reader.dataReader(stateNames, statePopulation, stateEducation, stateArea, stateActualPopulation, stateActualEducation, stateActualArea, regions1);
+    reader.dataReader(stateNames, regions1); //Calling the reader and gathering all the data from the reader
 
-    mOpenGLWidget->clearRegions();//clear the data first from the variables
+    mOpenGLWidget->clearRegions();
 
     switch (index) {
     case 1: // Handle Population selection
-    {        
+    {  
         // Pass the data to setupTableModel
-        for (int i = 0; i < stateActualPopulation.size(); i++) {
+        QStringList stateNames1;
+        QList<long> stateValues1;
+        for (int i = 0; i < stateNames.size(); i++) {
             stateNames1 << QString::fromStdString(stateNames[i]);
-            stateValues1 << stateActualPopulation[i];
+            stateValues1 << regions1[i].statePopulation;
         }
         setupTableModel(stateNames1, stateValues1);
 
         // Loop through the array and call addFilePoints for each region
         for (RegionInfo1& region : regions1) {
-            mOpenGLWidget->addFilePoints(region.filePath, region.r, region.g, region.b, region.statePopulation);
+            mOpenGLWidget->addFilePoints(region.filePath, region.r, region.g, region.b, (region.statePopulation / 10000000));
         }
         update();
         break;
@@ -122,15 +114,17 @@ void StatsOfIndia::handleComboBoxChange(int index) {
     case 2: // Handle Education selection
     { 
         // Pass the data to setupTableModel
-        for (int i = 0; i < stateActualEducation.size(); i++) {
-            stateNames1 << QString::fromStdString(stateNames[i]);
-            stateValues1 << stateActualEducation[i];
+        QStringList stateNames2;
+        QList<long> stateValues2;
+        for (int i = 0; i < stateNames.size(); i++) {
+            stateNames2 << QString::fromStdString(stateNames[i]);
+            stateValues2 << regions1[i].stateEducation ;
         }
-        setupTableModel(stateNames1, stateValues1);
+        setupTableModel(stateNames2, stateValues2);
 
         // Loop through the array and call addFilePoints for each region
         for (RegionInfo1& region : regions1) {
-            mOpenGLWidget->addFilePoints(region.filePath, region.r, region.g, region.b, region.stateEducation);
+            mOpenGLWidget->addFilePoints(region.filePath, region.r, region.g, region.b, (region.stateEducation - 60) / 2);
         }
         update();
         break;
@@ -139,15 +133,17 @@ void StatsOfIndia::handleComboBoxChange(int index) {
     case 3: // Handle Area selection
     {
         // Pass the data to setupTableModel
-        for (int i = 0; i < stateActualArea.size(); i++) {
-            stateNames1 << QString::fromStdString(stateNames[i]);
-            stateValues1 << stateActualArea[i];
+        QStringList stateNames3;
+        QList<long> stateValues3;
+        for (int i = 0; i < stateNames.size(); i++) {
+            stateNames3 << QString::fromStdString(stateNames[i]);
+            stateValues3 << regions1[i].stateArea ;
         }
-        setupTableModel(stateNames1, stateValues1);
+        setupTableModel(stateNames3, stateValues3);
 
         // Loop through the array and call addFilePoints for each region
         for (RegionInfo1& region : regions1) {
-            mOpenGLWidget->addFilePoints(region.filePath, region.r, region.g, region.b, region.stateArea);
+            mOpenGLWidget->addFilePoints(region.filePath, region.r, region.g, region.b, (region.stateArea / 20000));
         }
         update();
         break;
